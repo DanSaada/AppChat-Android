20cacfa25783a60615ccb4adb55db97c3600ee5e
Dan Saada <dansaada2@gmail.com>
2023-06-12 12:45:55 +0300

    first commit

---
+++ app/src/main/java/com/appchat/activities/ChatListActivity.java
@@ -0,0 +1,16 @@
+package com.appchat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appchat.R;
+
+public class ChatListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list);
+    }
+}
