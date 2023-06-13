51a258cbaca1eeda2e4dda9b7c1829033fcf04e0
OfirHelerman <ofirhelerman9@gmail.com>
2023-06-13 22:59:30 +0300

    implemented pages

---
+++ app/src/main/java/com/appchat/activities/signup.java
@@ -0,0 +1,16 @@
+package com.appchat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appchat.R;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
