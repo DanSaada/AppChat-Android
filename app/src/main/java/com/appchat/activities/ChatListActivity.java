601c043872c169ba72ed0e08876376a5d597419c
idaninbar <117938645+idaninbar@users.noreply.github.com>
2023-06-25 18:11:19 +0300

    Add contact editing

--- app/src/main/java/com/appchat/activities/ChatListActivity.java
+++ app/src/main/java/com/appchat/activities/ChatListActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.appchat.Adapters.ContactListAdapter;
+import com.appchat.OperationCallback;
 import com.appchat.R;
+import com.appchat.entities.Contact;
 import com.appchat.viewModels.ContactsViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
-public class ChatListActivity extends AppCompatActivity {
+public class ChatListActivity extends AppCompatActivity implements OperationCallback {
 
     private ContactsViewModel contactsViewModel;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +45,31 @@ public class ChatListActivity extends AppCompatActivity {
         // TODO: change addContactFragment into Activity
         fabAddChat.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(ChatListActivity.this);
-            final View customLayout = getLayoutInflater().inflate(R.layout.fragment_add_contact, null);
+            final View customLayout = getLayoutInflater().inflate(R.layout.activity_add_contact, null);
             builder.setView(customLayout);
-            AlertDialog dialog = builder.create();
+            dialog = builder.create();
             ImageButton exitBtn = customLayout.findViewById(R.id.Exit);
             exitBtn.setOnClickListener(v1 -> dialog.dismiss());
             Button addContactBtn = customLayout.findViewById(R.id.addButton);
             addContactBtn.setOnClickListener(v1 -> {
                 EditText usernameEditText = customLayout.findViewById(R.id.usernameEditText);
-                contactsViewModel.createChat(usernameEditText.getText().toString());
-                dialog.dismiss();
+                Contact contact = contactsViewModel.get(usernameEditText.getText().toString()).getValue();
+                if (contact != null) {
+                    contactsViewModel.setCallback(this);
+                    contactsViewModel.add(contact);
+                }
         });
         dialog.show();
         });
+    }
+
+    @Override
+    public void onSuccess() {
+        dialog.dismiss();
+    }
+
+    @Override
+    public void onFail() {
+        Toast.makeText(this, "Wrong Username", Toast.LENGTH_SHORT).show();
     }
 }
